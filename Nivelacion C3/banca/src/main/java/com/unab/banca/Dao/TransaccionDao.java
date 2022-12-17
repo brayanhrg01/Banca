5ea4901475ee3c50e4f01282ccdb56c2e74c9cf0
brayanhrg01 <113222580+brayanhrg01@users.noreply.github.com>
2022-12-17 18:35:09 -0500

    Add files via upload

---
+++ Nivelacion C3/banca/src/main/java/com/unab/banca/Dao/TransaccionDao.java
@@ -0,0 +1,7 @@
+package com.unab.banca.Dao;
+import com.unab.banca.Models.Transaccion;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface TransaccionDao extends CrudRepository< Transaccion, String>  {
+}
---
+++ Nivelacion C3/banca/src/main/java/com/unab/banca/Models/Transaccion.java
@@ -0,0 +1,34 @@
+package com.unab.banca.Models;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@Entity
+@Table(name="transaccion")
+public class Transaccion implements Serializable {
+    @Id
+    @Column(name="id_transaccion")
+    private String id_transaccion;
+    @Column(name="fecha_transaccion")
+    private Date fecha_transaccion;
+    @Column(name="valor_transaccion")
+    private double valor_transaccion;
+    @Column(name="tipo_transaccion")
+    private double tipo_transaccion;
+    @ManyToOne
+    @JoinColumn(name="id_cliente")
+    private Cliente cliente;
+    @Override
+    public String toString() {
+        return "Transaccion [id_transaccion=" + id_transaccion + ", fecha_transaccion=" + fecha_transaccion + ", valor_transaccion="
+                + valor_transaccion + ", tipo_transaccion" + tipo_transaccion + ", cliente=" + cliente + "]";
+    }
+}
