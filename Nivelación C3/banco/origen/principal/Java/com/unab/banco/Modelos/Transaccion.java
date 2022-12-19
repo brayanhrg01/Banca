ae45ace0606cb638d6d5aa2821a5bd99afc5ed54
brayanhrg01 <113222580+brayanhrg01@users.noreply.github.com>
2022-12-18 20:07:46 -0500

    Update and rename Nivelacion C3/banca/src/main/java/com/unab/banca/Models/Transaccion.java to Nivelación C3/banco/origen/principal/Java/com/unab/banco/Modelos/Transaccion.java

--- Nivelación C3/banco/origen/principal/Java/com/unab/banco/Modelos/Transaccion.java
+++ Nivelación C3/banco/origen/principal/Java/com/unab/banco/Modelos/Transaccion.java
@@ -21,14 +21,12 @@ public class Transaccion implements Serializable {
     private Date fecha_transaccion;
     @Column(name="valor_transaccion")
     private double valor_transaccion;
-    @Column(name="tipo_transaccion")
-    private double tipo_transaccion;
     @ManyToOne
     @JoinColumn(name="id_cliente")
     private Cliente cliente;
     @Override
     public String toString() {
         return "Transaccion [id_transaccion=" + id_transaccion + ", fecha_transaccion=" + fecha_transaccion + ", valor_transaccion="
-                + valor_transaccion + ", tipo_transaccion" + tipo_transaccion + ", cliente=" + cliente + "]";
+                + valor_transaccion +", cliente=" + cliente + "]";
     }
 }
